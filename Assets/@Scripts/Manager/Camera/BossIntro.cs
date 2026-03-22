efe32a44b6cceed83e1fe3b3e5c200ad3224c24c
owl3927 <heich4088@gmail.com>
2026-03-22 15:58:34 +0900

    보스 연출 이벤트 연결

---
+++ Assets/@Scripts/Manager/Camera/BossIntro.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class BossIntro : MonoBehaviour
+{
+    public static event Action OnEndIntro;
+
+    [SerializeField] private float _endTime = 4f;
+    [SerializeField] private CinemachineCamera _main;
+    [SerializeField] private CinemachineCamera _intro;
+
+    
+
+    private void OnEnable()
+    {
+        CameraManager.OnBossIntro += RunIntro;
+    }
+
+    private void OnDisable()
+    {
+        CameraManager.OnBossIntro -= RunIntro;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void RunIntro()
+    {
+        StartCoroutine(EndIntro());
+    }
+
+    IEnumerator EndIntro()
+    {
+        yield return new WaitForSeconds(_endTime);
+        _main.Priority = 10;
+        _intro.Priority = 5;
+        OnEndIntro?.Invoke();
+
+    }
+}
